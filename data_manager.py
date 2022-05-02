0182f02a7459e6527acacf2119e95eae207884ca
Teoflor <teodoranflorentina@gmail.com>
2022-05-02 14:56:39 +0300

    add connection.py

--- data_manager.py
+++ data_manager.py
@@ -1,12 +1,5 @@
-import csv
-import os
-
-QUESTIONS = os.getenv('QUESTIONS')
+import connection
 
 def list_questions():
-    questions = []
-    with open(QUESTIONS, "r") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            questions.append(row)
+    questions = connection.read_questions()
     return sorted(questions, key=lambda question:question['submission_time'])
