6493a3d89e3426cf6fdee5645377ee2af2f69cfd
Teoflor <teodoranflorentina@gmail.com>
2022-05-02 14:38:29 +0300

    add list question

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
+import data_manager
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
+@app.route("/list")
+def display_questions():
+    questions= data_manager.list_questions()
+    return render_template('list.html', questions=questions)
 
 if __name__ == "__main__":
     app.run()
