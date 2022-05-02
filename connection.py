9d043d60dd87dcdfab3c4a81edd5431b4144ce7b
Teoflor <teodoranflorentina@gmail.com>
2022-05-02 17:09:05 +0300

    add display a question functionality

--- connection.py
+++ connection.py
@@ -1,7 +1,8 @@
 import csv
 import os
 
-QUESTIONS = os.getenv('QUESTIONS')
+QUESTIONS = os.getenv('QUESTIONS') if 'QUESTIONS' in os.environ else 'question.csv'
+ANSWERS = os.getenv('ANSWERS') if 'ANSWERS' in os.environ else 'answer.csv'
 
 
 def read_questions():
@@ -11,3 +12,12 @@ def read_questions():
         for row in reader:
             questions.append(row)
     return questions
+
+def read_answers():
+    answers = []
+    with open(ANSWERS, "r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            answers.append(row)
+    return answers
+
--- server.py
+++ server.py
@@ -5,10 +5,17 @@ import data_manager
 app = Flask(__name__)
 
 
+@app.route("/")
 @app.route("/list")
 def display_questions():
     questions= data_manager.list_questions()
-    return render_template('list.html', questions=questions)
+    return render_template('list_of_questions.html', questions=questions)
+
+@app.route("/question/<question_id>")
+def display_question_by_id(question_id):
+    question = data_manager.get_question_by_id(question_id)
+    answers = data_manager.get_answers_by_question_id(question_id)
+    return render_template("individual_question.html", question=question, answers = answers)
 
 if __name__ == "__main__":
     app.run()
