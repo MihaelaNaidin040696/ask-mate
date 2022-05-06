dc78e5de4073d98e5b52a7b2555e93a8cf754397
alexsincai <alex.sincai@yahoo.co.uk>
2022-05-06 14:56:13 +0300

    formatting

--- util.py
+++ util.py
@@ -3,12 +3,14 @@ from datetime import datetime
 
 
 def get_id():
-    return max([int(question.get("id")) for question in connection.read_questions()]) + 1
+    return (
+        max([int(question.get("id")) for question in connection.read_questions()]) + 1
+    )
 
 
 def get_datetime_format(sec):
     datetime_format = datetime.fromtimestamp(float(sec) / 1000.0)
-    datetime_format=datetime_format.strftime("%B %d, %Y %I:%M")
+    datetime_format = datetime_format.strftime("%B %d, %Y %I:%M")
     return datetime_format
 
 
@@ -18,4 +20,3 @@ def get_now_datetime():
     now = datetime.strptime(now, "%d/%m/%Y %H:%M:%S:%fff")
     current_time = now.timestamp() * 1000
     return current_time
-
