7c9f9a31b3d29b0fa342f173036c1c93481b43d3
alexsincai <alex.sincai@yahoo.co.uk>
2022-05-06 15:07:25 +0300

    extracted code into functions

--- connection.py
+++ connection.py
@@ -1,102 +1,110 @@
 import csv
 import os
 
-QUESTIONS = os.getenv('QUESTIONS') if 'QUESTIONS' in os.environ else 'question.csv'
-ANSWERS = os.getenv('ANSWERS') if 'ANSWERS' in os.environ else 'answer.csv'
-QUESTIONS_HEADERS = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
-ANSWERS_HEADERS = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
-UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER') if 'UPLOAD_FOLDER' in os.environ else 'images'
+QUESTIONS = os.path.join(os.path.dirname(__file__), "sample_data", "question.csv")
+ANSWERS = os.path.join(os.path.dirname(__file__), "sample_data", "answer.csv")
+
+QUESTIONS_HEADERS = [
+    "id",
+    "submission_time",
+    "view_number",
+    "vote_number",
+    "title",
+    "message",
+    "image",
+]
+ANSWERS_HEADERS = [
+    "id",
+    "submission_time",
+    "vote_number",
+    "question_id",
+    "message",
+    "image",
+]
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "static", "images")
+
+
+def read_file(file):
+    with open(file, "r") as f:
+        reader = csv.DictReader(f)
+        return list(reader)
 
 
 def read_questions():
-    questions = []
-    with open(QUESTIONS, "r") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            questions.append(row)
-    return questions
+    return read_file(QUESTIONS)
 
 
 def read_answers():
-    answers = []
-    with open(ANSWERS, "r") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            answers.append(row)
-    return answers
+    return read_file(ANSWERS)
+
+
+def write_file(file, headers, data):
+    with open(file, "a") as f:
+        writer = csv.DictWriter(f, headers)
+        writer.writerow(data)
 
 
 def write_new_question(question):
-    with open(QUESTIONS, "a") as f:
-        writer = csv.DictWriter(f, QUESTIONS_HEADERS)
-        writer.writerow(question)
+    write_file(QUESTIONS, QUESTIONS_HEADERS, question)
 
 
 def write_new_answer(answer):
-    with open(ANSWERS, "a") as f:
-        writer = csv.DictWriter(f, ANSWERS_HEADERS)
-        writer.writerow(answer)
+    write_file(ANSWERS, ANSWERS_HEADERS, answer)
+
+
+def rewrite_file(file, headers, data):
+    with open(file, "w") as f:
+        writer = csv.DictWriter(f, headers)
+        writer.writeheader()
+        for item in data:
+            writer.writerow(item)
 
 
 def delete_question(question_to_delete):
     questions = read_questions()
     questions.remove(question_to_delete)
-    with open(QUESTIONS, "w") as f:
-        writer = csv.DictWriter(f, QUESTIONS_HEADERS)
-        writer.writeheader()
-        for question in questions:
-            writer.writerow(question)
+
+    rewrite_file(QUESTIONS, QUESTIONS_HEADERS, questions)
 
 
 def rewrite_questions(question_list):
-    with open(QUESTIONS, "w") as f:
-        writer = csv.DictWriter(f, QUESTIONS_HEADERS)
-        writer.writeheader()
-        for question in question_list:
-            writer.writerow(question)
+    rewrite_file(QUESTIONS, QUESTIONS_HEADERS, question_list)
 
 
 def rewrite_answers(answer_list):
-    with open(ANSWERS, "w") as f:
-        writer = csv.DictWriter(f, ANSWERS_HEADERS)
-        writer.writeheader()
-        for answer in answer_list:
-            writer.writerow(answer)
+    rewrite_file(ANSWERS, ANSWERS_HEADERS, answer_list)
+
+
+def delete_image_from_answer(image):
+    if image:
+        path = os.path.join(os.path.dirname(UPLOAD_FOLDER), "images", image)
+        os.remove(path)
 
 
 def delete_answers_by_question_id(question_id):
-    answers = read_answers()
     new_answers = []
-    for answer in answers:
-        if answer['question_id'] == question_id:
-            if answer['image']:
-                path = os.path.join(os.path.dirname(UPLOAD_FOLDER), 'images', answer['image'])
-                os.remove(path)
-        if answer['question_id'] != question_id:
+
+    for answer in read_answers():
+        if answer["question_id"] == question_id:
+            delete_image_from_answer(answer.get("image"))
+
+        if answer["question_id"] != question_id:
             new_answers.append(answer)
-    with open(ANSWERS, "w") as f:
-        writer = csv.DictWriter(f, ANSWERS_HEADERS)
-        writer.writeheader()
-        for answer in new_answers:
-            writer.writerow(answer)
+
+    rewrite_file(ANSWERS, ANSWERS_HEADERS, new_answers)
 
 
 def delete_answers_by_answer_id(answer_id):
     question_id = ""
-    answers = read_answers()
     new_answers = []
-    for answer in answers:
-        if answer['id'] != answer_id:
+
+    for answer in read_answers():
+        if answer["id"] != answer_id:
             new_answers.append(answer)
         else:
-            if answer['image']:
-                path = os.path.join(os.path.dirname(UPLOAD_FOLDER), 'images', answer['image'])
-                os.remove(path)
-            question_id = answer['question_id']
-    with open(ANSWERS, "w") as f:
-        writer = csv.DictWriter(f, ANSWERS_HEADERS)
-        writer.writeheader()
-        for answer in new_answers:
-            writer.writerow(answer)
-    return question_id
+            delete_image_from_answer(answer.get("image"))
+            question_id = answer["question_id"]
 
+    rewrite_file(ANSWERS, ANSWERS_HEADERS, new_answers)
+
+    return question_id
