175b5c156de9f0f580c6e2d2882615ec185bde5a
blackcat2-code <iusti_iustina1999@yahoo.com>
2022-05-16 17:13:51 +0300

    push new changes

--- server.py
+++ server.py
@@ -21,7 +21,8 @@ def display_questions():
 
 @app.route("/question/<question_id>")
 def display_question_by_id(question_id):
-    question_id = request.args.get('id')
+    question_id = request.args.get("id")
+    print(question_id)
     if question_id:
         question = data_manager.get_question_by_id(question_id)
         answers = data_manager.get_answers_by_question_id(question_id)
