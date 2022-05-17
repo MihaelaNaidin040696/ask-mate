87b2e0d7c12c370b3130c6fc82a3ab4a3ff7adef
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2022-05-17 12:09:07 +0300

    added add new answer and delete question functionality

--- server.py
+++ server.py
@@ -27,44 +27,25 @@ def display_question_by_id(question_id):
         "individual_question.html",
         question=question,
         answers=answers,
+        question_id=question_id,
     )
 
 
 @app.route("/add-question", methods=["GET", "POST"])
 def add_new_question():
-    if request.method == 'POST':
+    if request.method == "POST":
         image = ""
         file = request.files["image"]
         if file:
             file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
             image = file.filename
-        data_manager.write_question(request.form.get("title").capitalize(), request.form.get("message").capitalize(), image)
+        data_manager.write_question(
+            request.form.get("title").capitalize(),
+            request.form.get("message").capitalize(),
+            image,
+        )
         return redirect(url_for("display_question_by_id", question_id=id))
     return render_template("add_question.html")
-
-#     if request.method == "POST":
-#         id = util.get_id()
-#         image = ""
-#
-#         file = request.files["image"]
-#         if file:
-#             file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
-#             image = file.filename
-#
-#         data_manager.write_question(
-#             {
-#                 "id": id,
-#                 "submission_time": util.get_now_datetime(),
-#                 "view_number": 0,
-#                 "vote_number": 0,
-#                 "title": request.form.get("title").capitalize(),
-#                 "message": request.form.get("message").capitalize(),
-#                 "image": image,
-#             }
-#         )
-#         return redirect(url_for("display_question_by_id", question_id=id))
-#     return render_template("add_question.html")
-    pass
 
 
 @app.route("/question/<question_id>/new-answer", methods=["GET", "POST"])
@@ -75,63 +56,61 @@ def add_new_answer(question_id):
         if file:
             file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
             image = file.filename
-        data_manager.write_answer(question_id, request.form.get("message").capitalize(), image)
-        return redirect(url_for("display_question_by_id", question_id=id))
+        data_manager.write_answer(
+            question_id, request.form.get("message").capitalize(), image
+        )
+        return redirect(url_for("display_question_by_id", question_id=question_id))
     return render_template("add_answer.html", question_id=question_id)
-#
 
-@app.route("/question/<question_id>/delete")
+
+@app.route("/question/<question_id>/delete", methods=["GET", "POST"])
 def delete_questions(question_id):
-#     data_manager.delete_question(question_id)
-#     return redirect(url_for("display_questions"))
-#
-#
-# @app.route("/question/<question_id>/edit", methods=["GET", "POST"])
-# def edit_question(question_id):
-#     questions = data_manager.list_questions()
-#     entry = [question for question in questions if question["id"] == question_id][0]
-#
-#     if request.method == "POST":
-#         for question in questions:
-#             if question["id"] == question_id:
-#                 question["title"] = request.form.get("title").capitalize()
-#                 question["message"] = request.form.get("message").capitalize()
-#
-#         data_manager.rewrite_questions(questions)
-#         return redirect(url_for("display_question_by_id", question_id=question_id))
-#
-#     return render_template("edit_question.html", question=entry)
-    pass
-#
+    questions = data_manager.get_questions()
+    for question in questions:
+        if int(question_id) == question["id"]:
+            data_manager.delete_question(question_id)
+    return redirect(url_for("display_questions", question_id=question_id))
+
+
 @app.route("/answer/<answer_id>/delete")
 def delete_answers(answer_id):
-#     question_id = data_manager.delete_answer(answer_id)
-#     return redirect(url_for("display_question_by_id", question_id=question_id))
+    #     question_id = data_manager.delete_answer(answer_id)
+    #     return redirect(url_for("display_question_by_id", question_id=question_id))
     pass
+
+
 #
 @app.route("/question/<question_id>/vote-up")
 def vote_up_question(question_id):
-#     data_manager.vote_up_question(question_id)
-#     return redirect(url_for("display_questions"))
+    #     data_manager.vote_up_question(question_id)
+    #     return redirect(url_for("display_questions"))
     pass
+
+
 #
 @app.route("/question/<question_id>/vote-down")
 def vote_down_question(question_id):
-#     data_manager.vote_down_question(question_id)
-#     return redirect(url_for("display_questions"))
+    #     data_manager.vote_down_question(question_id)
+    #     return redirect(url_for("display_questions"))
     pass
+
+
 #
 @app.route("/answer/<answer_id>/vote-up")
 def vote_up_answer(answer_id):
-#     question_id = data_manager.vote_up_answer(answer_id)
-#     return redirect(url_for("display_question_by_id", question_id=question_id))
+    #     question_id = data_manager.vote_up_answer(answer_id)
+    #     return redirect(url_for("display_question_by_id", question_id=question_id))
     pass
+
+
 #
 @app.route("/answer/<answer_id>/vote-down")
 def vote_down_answer(answer_id):
-#     question_id = data_manager.vote_down_answer(answer_id)
-#     return redirect(url_for("display_question_by_id", question_id=question_id))
+    #     question_id = data_manager.vote_down_answer(answer_id)
+    #     return redirect(url_for("display_question_by_id", question_id=question_id))
     pass
+
+
 #
 if __name__ == "__main__":
     app.run(debug=True)
