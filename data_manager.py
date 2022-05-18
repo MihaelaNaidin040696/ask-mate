62531ff527f384b69de029fe59db74328a39bf1b
Teoflor <teodoranflorentina@gmail.com>
2022-05-18 09:50:00 +0300

    merge Teo Mihaela

--- data_manager.py
+++ data_manager.py
@@ -4,95 +4,140 @@ import database_common
 
 @database_common.connection_handler
 def get_questions(cursor: RealDictCursor) -> list:
-    query = """select * from question order by submission_time"""
-    cursor.execute(query)
+    cursor.execute(
+        """
+        SELECT * 
+        FROM question 
+        ORDER BY submission_time"""
+    )
     return cursor.fetchall()
 
 
 @database_common.connection_handler
-def get_question_by_id(cursor,id):
-    query=f"""
+def get_question_by_id(cursor: RealDictCursor, id) -> list:
+    cursor.execute(
+        """
         SELECT title, message
         FROM question
-        WHERE id = {id}"""
-    cursor.execute(query)
+        WHERE id = %(id)s;""",
+        {"id": id},
+    )
     return cursor.fetchone()
 
 
 @database_common.connection_handler
 def get_answers_by_question_id(cursor: RealDictCursor, id) -> list:
-    query = """select * from answer where question_id = id"""
-    value = {"id": id}
-    cursor.execute(query, value)
+    cursor.execute(
+        """
+        SELECT * 
+        FROM answer 
+        WHERE question_id = %(id)s;""",
+        {"id": id},
+    )
     return cursor.fetchall()
 
 
 @database_common.connection_handler
 def sort_questions(cursor: RealDictCursor, criteria, direction) -> list:
-    query =  f"""select * from question order by {criteria} {direction}"""
+    query = f"""
+        SELECT * 
+        FROM question 
+        ORDER BY {criteria} {direction}"""
     cursor.execute(query)
     return cursor.fetchall()
 
 
 @database_common.connection_handler
 def write_question(cursor, title, message, image):
-    query = f"""
+    cursor.execute(
+        """
     INSERT INTO question (submission_time,view_number, vote_number, title, message, image)
-    VALUES (now(), 0, 0, '{title}', '{message}', '{image}')"""
-    cursor.execute(query)
+    VALUES (now()::timestamp(0), 0, 0, %(title)s, %(message)s, %(image)s);""",
+        {"title": title, "message": message, "image": image},
+    )
 
-#     connection.write_new_question(new_question)
-#
-#
 
-#
-#
-# def write_answer(new_answer):
-#     connection.write_new_answer(new_answer)
-#
-#
-#
-#
-# def delete_question(question_id):
-#     question = get_question_by_id(question_id)
-#     connection.delete_image_from_file(question["image"])
-#     connection.delete_answers_by_question_id(question_id)
-#     connection.delete_question(question)
-#
-#
-# def delete_answer(answer_id):
-#     return connection.delete_answers_by_answer_id(answer_id)
-#
-#
-# def vote_question(id, modifier):
-#     questions = connection.read_questions()
-#     for question in questions:
-#         if question["id"] == id:
-#             question["vote_number"] = int(question["vote_number"]) + modifier
-#     connection.rewrite_questions(questions)
-#
-#
-# def vote_answer(id, modifier):
-#     answers = connection.read_answers()
-#     for answer in answers:
-#         if answer["id"] == id:
-#             answer["vote_number"] = int(answer["vote_number"]) + modifier
-#             question_id = answer["question_id"]
-#             connection.rewrite_answers(answers)
-#             return question_id
-#
-#
-# def vote_up_question(id):
-#     vote_question(id, 1)
-#
-#
-# def vote_down_question(id):
-#     vote_question(id, -1)
-#
-#
-# def vote_up_answer(id):
-#     return vote_answer(id, 1)
-#
-#
-# def vote_down_answer(id):
-#     return vote_answer(id, -1)
+@database_common.connection_handler
+def write_answer(cursor: RealDictCursor, question_id, message, image):
+    cursor.execute(
+        """
+        INSERT INTO answer (submission_time, vote_number, question_id, message, image)
+        VALUES (now()::timestamp(0), 0, %(question_id)s, %(message)s, %(image)s);""",
+        {"question_id": question_id, "message": message, "image": image},
+    )
+
+
+@database_common.connection_handler
+def delete_question(cursor: RealDictCursor, id):
+    print(
+        cursor.execute(
+            """
+            DELETE FROM comment WHERE question_id = %(id)s;
+            DELETE FROM answer WHERE question_id = %(id)s;
+            DELETE FROM question_tag WHERE question_id = %(id)s;
+            DELETE FROM question WHERE id = %(id)s;""",
+            {"id": id},
+        )
+    )
+
+
+@database_common.connection_handler
+def delete_answer(cursor: RealDictCursor, id):
+    print(
+        cursor.execute(
+            """
+            DELETE FROM comment WHERE answer_id = %(id)s;
+            DELETE FROM answer WHERE id = %(id)s;""",
+            {"id": id},
+        )
+    )
+
+
+@database_common.connection_handler
+def edit_question(cursor: RealDictCursor, id, title, message):
+    cursor.execute(
+        """
+        UPDATE question SET title = %(title)s, message = %(message)s
+        WHERE id = %(id)s;""",
+        {"id": id, "title": title, "message": message},
+    )
+
+
+@database_common.connection_handler
+def vote_up_question(cursor: RealDictCursor, id):
+    cursor.execute(
+        """
+        UPDATE question SET vote_number = vote_number + 1
+        WHERE id = %(id)s;""",
+        {"id": id},
+    )
+
+
+@database_common.connection_handler
+def vote_down_question(cursor: RealDictCursor, id):
+    cursor.execute(
+        """
+        UPDATE question SET vote_number = vote_number - 1
+        WHERE id = %(id)s;""",
+        {"id": id},
+    )
+
+
+@database_common.connection_handler
+def vote_up_answer(cursor: RealDictCursor, id):
+    cursor.execute(
+        """
+        UPDATE answer SET vote_number = vote_number + 1
+        WHERE id = %(id)s;""",
+        {"id": id},
+    )
+
+
+@database_common.connection_handler
+def vote_down_answer(cursor: RealDictCursor, id):
+    cursor.execute(
+        """
+        UPDATE answer SET vote_number = vote_number - 1
+        WHERE id = %(id)s;""",
+        {"id": id},
+    )
