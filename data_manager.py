b91dd72d49a93f75f279f90cc152721180fedca3
alexsincai <alex.sincai@yahoo.co.uk>
2022-05-23 11:55:31 +0300

    queries reformatted, function extracted

--- data_manager.py
+++ data_manager.py
@@ -3,46 +3,30 @@ import database_common
 
 
 @database_common.connection_handler
-def get_headers(cursor: RealDictCursor) -> list:
-    cursor.execute(
-        """
-        SELECT * 
-        FROM question 
-        ORDER BY submission_time"""
-    )
+def get_headers(cursor):
+    cursor.execute("SELECT * FROM question ORDER BY submission_time;")
     return cursor.fetchone()
 
 
 @database_common.connection_handler
-def get_questions(cursor: RealDictCursor) -> list:
-    cursor.execute(
-        """
-        SELECT * 
-        FROM question 
-        ORDER BY submission_time"""
-    )
+def get_questions(cursor):
+    cursor.execute("SELECT * FROM question ORDER BY submission_time;")
     return cursor.fetchall()
 
 
 @database_common.connection_handler
-def get_question_by_id(cursor: RealDictCursor, id) -> list:
+def get_question_by_id(cursor, id):
     cursor.execute(
-        """
-        SELECT title, message
-        FROM question
-        WHERE id = %(id)s;""",
+        "SELECT title, message FROM question WHERE id = %(id)s;",
         {"id": id},
     )
     return cursor.fetchone()
 
 
 @database_common.connection_handler
-def get_answer_id_by_question_id(cursor: RealDictCursor, id) -> list:
+def get_answer_id_by_question_id(cursor, id):
     cursor.execute(
-        """
-        SELECT id
-        FROM answer
-        WHERE question_id = %(id)s;""",
+        "SELECT id FROM answer WHERE question_id = %(id)s;",
         {"id": id},
     )
     answer_dict = cursor.fetchall()
@@ -50,45 +34,39 @@ def get_answer_id_by_question_id(cursor: RealDictCursor, id) -> list:
 
 
 @database_common.connection_handler
-def get_answers_by_question_id(cursor: RealDictCursor, id) -> list:
+def get_answers_by_question_id(cursor, id):
     cursor.execute(
-        """
-        SELECT * 
-        FROM answer 
-        WHERE question_id = %(id)s;""",
+        "SELECT * FROM answer WHERE question_id = %(id)s;",
         {"id": id},
     )
     return cursor.fetchall()
 
 
 @database_common.connection_handler
-def get_answers_by_answer_id(cursor: RealDictCursor, id: int) -> list:
+def get_answers_by_answer_id(cursor, id):
     cursor.execute(
-        f"""
-        SELECT * 
-        FROM answer 
-        WHERE id = {id};"""
+        "SELECT * FROM answer WHERE id = %(id)s;",
+        {"id": id},
     )
     return cursor.fetchone()
 
 
 @database_common.connection_handler
-def get_comments_by_comment_id(cursor: RealDictCursor, id: int) -> list:
+def get_comments_by_comment_id(cursor, id):
     cursor.execute(
-        f"""
-        SELECT * 
-        FROM comment 
-        WHERE id = {id};"""
+        "SELECT * FROM comment WHERE id = %(id)s;",
+        {"id": id},
     )
     return cursor.fetchone()
 
 
 @database_common.connection_handler
-def sort_questions(cursor: RealDictCursor, criteria, direction) -> list:
+def sort_questions(cursor, criteria, direction):
     query = f"""
         SELECT * 
         FROM question 
-        ORDER BY {criteria} {direction}"""
+        ORDER BY {criteria} {direction};
+        """
     cursor.execute(query)
     return cursor.fetchall()
 
@@ -97,272 +75,249 @@ def sort_questions(cursor: RealDictCursor, criteria, direction) -> list:
 def write_question(cursor, title, message, image):
     cursor.execute(
         """
-    INSERT INTO question (submission_time,view_number, vote_number, title, message, image)
-    VALUES (now()::timestamp(0), 0, 0, %(title)s, %(message)s, %(image)s) returning id;""",
-        {"title": title, "message": message, "image": image},
+            INSERT INTO question (submission_time, view_number, vote_number, title, message, image)
+            VALUES (now()::timestamp(0), 0, 0, %(title)s, %(message)s, %(image)s) RETURNING id;
+        """,
+        {
+            "title": title,
+            "message": message,
+            "image": image,
+        },
     )
     return cursor.fetchone()
 
 
 @database_common.connection_handler
-def write_answer(cursor: RealDictCursor, question_id, message, image):
+def write_answer(cursor, question_id, message, image):
     cursor.execute(
         """
-        INSERT INTO answer (submission_time, vote_number, question_id, message, image)
-        VALUES (now()::timestamp(0), 0, %(question_id)s, %(message)s, %(image)s);""",
-        {"question_id": question_id, "message": message, "image": image},
+            INSERT INTO answer (submission_time, vote_number, question_id, message, image)
+            VALUES (now()::timestamp(0), 0, %(question_id)s, %(message)s, %(image)s);
+        """,
+        {
+            "question_id": question_id,
+            "message": message,
+            "image": image,
+        },
     )
 
 
 @database_common.connection_handler
-def delete_question(cursor: RealDictCursor, id):
-    print(
-        cursor.execute(
-            """
+def delete_question(cursor, id):
+    cursor.execute(
+        """
             DELETE FROM comment WHERE question_id = %(id)s;
             DELETE FROM answer WHERE question_id = %(id)s;
             DELETE FROM question_tag WHERE question_id = %(id)s;
-            DELETE FROM question WHERE id = %(id)s;""",
-            {"id": id},
-        )
+            DELETE FROM question WHERE id = %(id)s;
+        """,
+        {"id": id},
     )
 
 
 @database_common.connection_handler
-def delete_answer(cursor: RealDictCursor, id):
+def delete_answer(cursor, id):
     cursor.execute(
         """
             DELETE FROM comment WHERE answer_id = %(id)s;
-            DELETE FROM answer WHERE id = %(id)s;""",
+            DELETE FROM answer WHERE id = %(id)s;
+        """,
         {"id": id},
     )
 
 
 @database_common.connection_handler
-def edit_question(cursor: RealDictCursor, id, title, message):
+def edit_question(cursor, id, title, message):
     cursor.execute(
-        """
-        UPDATE question SET title = %(title)s, message = %(message)s
-        WHERE id = %(id)s;""",
-        {"id": id, "title": title, "message": message},
+        "UPDATE question SET title = %(title)s, message = %(message)s WHERE id = %(id)s;",
+        {
+            "id": id,
+            "title": title,
+            "message": message,
+        },
     )
 
 
 @database_common.connection_handler
-def vote_up_question(cursor: RealDictCursor, id):
+def vote_item(cursor, table, id, modifier):
     cursor.execute(
-        """
-        UPDATE question SET vote_number = vote_number + 1
-        WHERE id = %(id)s;""",
-        {"id": id},
+        f"UPDATE {table} SET vote_number = vote_number + %(modifier)s WHERE id = %(id)s;",
+        {
+            "id": id,
+            "modifier": modifier,
+        },
     )
 
 
-@database_common.connection_handler
-def vote_down_question(cursor: RealDictCursor, id):
-    cursor.execute(
-        """
-        UPDATE question SET vote_number = vote_number - 1
-        WHERE id = %(id)s;""",
-        {"id": id},
-    )
+def vote_up_question(id):
+    vote_item(id, "question", 1)
+
+
+def vote_down_question(id):
+    vote_item(id, "question", -1)
+
+
+def vote_up_answer(id):
+    vote_item(id, "answer", 1)
+
+
+def vote_down_answer(id):
+    vote_item(id, "answer", -1)
 
 
 @database_common.connection_handler
-def vote_up_answer(cursor: RealDictCursor, id):
+def get_question_comments(cursor, id):
     cursor.execute(
-        """
-        UPDATE answer SET vote_number = vote_number + 1
-        WHERE id = %(id)s ;""",
-        {"id": id},
-    )
-
-
-@database_common.connection_handler
-def vote_down_answer(cursor: RealDictCursor, id):
-    cursor.execute(
-        """
-        UPDATE answer SET vote_number = vote_number - 1
-        WHERE id = %(id)s;""",
-        {"id": id},
-    )
-
-
-@database_common.connection_handler
-def get_question_comments(cursor: RealDictCursor, id) -> list:
-    cursor.execute(
-        """
-        SELECT *
-        FROM comment
-        WHERE question_id = %(id)s;""",
+        "SELECT * FROM comment WHERE question_id = %(id)s;",
         {"id": id},
     )
     return cursor.fetchall()
 
 
 @database_common.connection_handler
-def get_comments(cursor: RealDictCursor) -> list:
-    cursor.execute(
-        """
-        SELECT *
-        FROM comment;"""
-    )
+def get_comments(cursor):
+    cursor.execute("SELECT * FROM comment;")
     return cursor.fetchall()
 
 
 @database_common.connection_handler
-def add_question_comment(cursor: RealDictCursor, id, message):
+def add_question_comment(cursor, id, message):
     cursor.execute(
         """
-        INSERT INTO comment (question_id, message, submission_time, edited_count)
-        VALUES (%(id)s, %(message)s, now()::timestamp(0), 0);""",
-        {"id": id, "message": message},
+            INSERT INTO comment (question_id, message, submission_time, edited_count)
+            VALUES (%(id)s, %(message)s, now()::timestamp(0), 0);
+        """,
+        {
+            "id": id,
+            "message": message,
+        },
     )
 
 
 @database_common.connection_handler
-def add_answer_comment(cursor: RealDictCursor, id, message):
+def add_answer_comment(cursor, id, message):
     cursor.execute(
         """
-        INSERT INTO comment (answer_id, message, submission_time, edited_count)
-        VALUES (%(id)s, %(message)s, now()::timestamp(0), 0);""",
-        {"id": id, "message": message},
+            INSERT INTO comment (answer_id, message, submission_time, edited_count)
+            VALUES (%(id)s, %(message)s, now()::timestamp(0), 0);
+        """,
+        {
+            "id": id,
+            "message": message,
+        },
     )
 
 
 @database_common.connection_handler
-def get_id_question_by_id_answer(cursor: RealDictCursor, answer_id):
+def get_id_for_item(cursor, item, table, id):
     cursor.execute(
-        """
-        SELECT question_id FROM answer
-        WHERE id = %(answer_id)s;""",
-        {"answer_id": answer_id},
+        f"SELECT {item} FROM {table} WHERE id = %(id)s;",
+        {"id": id},
     )
     answer_dict = cursor.fetchone()
-    return answer_dict["question_id"]
+    return answer_dict[item]
+
+
+def get_id_question_by_id_answer(answer_id):
+    return get_id_for_item("question_id", "answer", answer_id)
+
+
+def get_id_question_by_id_comment(comment_id):
+    return get_id_for_item("question_id", "comment", comment_id)
+
+
+def get_id_answer_by_id_comment(comment_id):
+    return get_id_for_item("answer_id", "comment", comment_id)
 
 
 @database_common.connection_handler
-def get_id_question_by_id_comment(cursor: RealDictCursor, comment_id):
+def edit_answer(cursor, id, message):
     cursor.execute(
-        """
-        SELECT question_id FROM comment
-        WHERE id = %(comment_id)s;""",
-        {"comment_id": comment_id},
-    )
-    answer_dict = cursor.fetchone()
-    return dict(answer_dict)["question_id"]
-
-
-@database_common.connection_handler
-def get_id_answer_by_id_comment(cursor: RealDictCursor, comment_id):
-    cursor.execute(
-        """
-        SELECT answer_id FROM comment
-        WHERE id = %(comment_id)s;""",
-        {"comment_id": comment_id},
-    )
-    answer_dict = cursor.fetchone()
-    return dict(answer_dict)["answer_id"]
-
-
-@database_common.connection_handler
-def edit_answer(cursor: RealDictCursor, id, message):
-    cursor.execute(
-        """
-        UPDATE answer SET message = %(message)s, submission_time = now()::timestamp(0) 
-        WHERE id = %(id)s;""",
-        {"id": id, "message": message},
+        "UPDATE answer SET message = %(message)s, submission_time = now()::timestamp(0) WHERE id = %(id)s;",
+        {
+            "id": id,
+            "message": message,
+        },
     )
 
 
 @database_common.connection_handler
-def edit_comment(cursor: RealDictCursor, id, message):
+def edit_comment(cursor, id, message):
     cursor.execute(
-        """
-        UPDATE comment 
-        SET message = %(message)s, 
-            submission_time = now()::timestamp(0),
-            edited_count = edited_count + 1
-        WHERE id = %(id)s;""",
-        {"id": id, "message": message},
+        "UPDATE comment SET message = %(message)s, submission_time = now()::timestamp(0), edited_count = edited_count + 1 WHERE id = %(id)s;",
+        {
+            "id": id,
+            "message": message,
+        },
     )
 
 
 @database_common.connection_handler
-def get_tags(cursor: RealDictCursor):
-    cursor.execute(
-        """
-        SELECT name 
-        FROM tag"""
-    )
+def get_tags(cursor):
+    cursor.execute("SELECT name FROM tag;")
     return cursor.fetchall()
 
 
 @database_common.connection_handler
 def get_tag_id(cursor, name):
-    cursor.execute("SELECT id FROM tag WHERE name = %(name)s;", {"name": name})
+    cursor.execute(
+        "SELECT id FROM tag WHERE name = %(name)s;",
+        {"name": name},
+    )
     return cursor.fetchone()
 
 
 @database_common.connection_handler
 def get_tags_by_question_id(cursor, question_id):
     cursor.execute(
-        """
-        SELECT * FROM tag
-        JOIN question_tag qt on tag.id = qt.tag_id
-        WHERE qt.question_id=%(question_id)s""",
+        "SELECT * FROM tag JOIN question_tag qt ON tag.id = qt.tag_id WHERE qt.question_id = %(question_id)s;",
         {"question_id": question_id},
     )
     return cursor.fetchall()
 
 
 @database_common.connection_handler
-def add_new_tag(cursor: RealDictCursor, name):
+def add_new_tag(cursor, name):
     cursor.execute(
-        """
-    INSERT INTO tag( name )
-    VALUES(%(name)s)
-    """,
+        "INSERT INTO tag(name) VALUES (%(name)s);",
         {"name": name},
     )
 
 
 @database_common.connection_handler
-def delete_comment(cursor: RealDictCursor, id):
+def delete_comment(cursor, id):
     cursor.execute(
-        """
-        DELETE FROM comment 
-        WHERE id = %(id)s;""",
+        "DELETE FROM comment WHERE id = %(id)s;",
         {"id": id},
     )
 
 
 @database_common.connection_handler
-def add_question_tag(cursor: RealDictCursor, question_id, tag_id):
+def add_question_tag(cursor, question_id, tag_id):
     cursor.execute(
-        """
-        INSERT INTO question_tag (question_id, tag_id)
-        VALUES(%(question_id)s, %(tag_id)s)""",
-        {"question_id": question_id, "tag_id": tag_id},
+        "INSERT INTO question_tag (question_id, tag_id) VALUES (%(question_id)s, %(tag_id)s);",
+        {
+            "question_id": question_id,
+            "tag_id": tag_id,
+        },
     )
 
 
 @database_common.connection_handler
 def delete_tag(cursor, question_id, tag_id):
     cursor.execute(
-        """    
-    DELETE FROM question_tag WHERE question_id = %(question_id)s and tag_id = %(tag_id)s""",
-        {"tag_id": tag_id, "question_id": question_id},
+        "DELETE FROM question_tag WHERE question_id = %(question_id)s and tag_id = %(tag_id)s;",
+        {
+            "tag_id": tag_id,
+            "question_id": question_id,
+        },
     )
 
 
 @database_common.connection_handler
 def get_data_for_search_question(cursor, search):
     cursor.execute(
-        """
-        SELECT * FROM question
-        WHERE title ilike %(search)s or message ilike %(search)s or image ilike %(search)s;""",
+        "SELECT * FROM question WHERE title ILIKE %(search)s OR message ILIKE %(search)s OR image ILIKE %(search)s;",
         {"search": f"%{search}%"},
     )
     return cursor.fetchall()
@@ -371,9 +326,7 @@ def get_data_for_search_question(cursor, search):
 @database_common.connection_handler
 def get_data_for_search_answer(cursor, search):
     cursor.execute(
-        """
-        SELECT * FROM answer
-        WHERE message ilike %(search)s or image ilike %(search)s;""",
+        "SELECT * FROM answer WHERE message ILIKE %(search)s or image ILIKE %(search)s;",
         {"search": f"%{search}%"},
     )
     return cursor.fetchall()
@@ -382,10 +335,6 @@ def get_data_for_search_answer(cursor, search):
 @database_common.connection_handler
 def get_latest_questions(cursor):
     cursor.execute(
-        """
-        SELECT submission_time, title, message 
-        FROM question
-        ORDER BY submission_time
-        LIMIT 5"""
+        "SELECT submission_time, title, message FROM question ORDER BY submission_time LIMIT 5;"
     )
     return cursor.fetchall()
