8f4294d7d51cc9b4fc8e8420620b9481621e593c
alexsincai <alex.sincai@yahoo.co.uk>
2022-06-03 13:30:37 +0300

    pw hashing

--- hash_pass.py
+++ hash_pass.py
@@ -2,10 +2,14 @@ import bcrypt
 
 
 def hash_password(plain_text_password):
-    hashed_bytes = bcrypt.hashpw(plain_text_password.encode("utf-8"), bcrypt.gensalt())
-    return hashed_bytes.decode("utf-8")
+    return bcrypt.hashpw(
+        plain_text_password.encode("utf-8"),
+        bcrypt.gensalt(),
+    ).decode("utf-8")
 
 
 def verify_password(plain_text_password, hashed_password):
-    hashed_bytes_password = hashed_password.encode("utf-8")
-    return bcrypt.checkpw(plain_text_password.encode("utf-8"), hashed_bytes_password)
+    return bcrypt.checkpw(
+        plain_text_password.encode("utf-8"),
+        hashed_password.encode("utf-8"),
+    )
